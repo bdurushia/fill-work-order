21e0c49e75c7364cfb85cde3fc3f52d4f3ebd035
bdurushia <b.durushia@gmail.com>
2022-02-11 23:53:41 -0500

    read form and get field names

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 const { PDFDocument } = require('pdf-lib');
 const { inquirer } = require('inquirer');
 const { readFile, writeFile } = require('fs/promises');
+// const { fs } = require('fs');
+// const workOrderForm = require('./form/work-order-form.pdf');
 
 async function createPdf(input, output) {
     try {
         const pdfDoc = await PDFDocument.load(await readFile(input));
 
         // Modify doc, fill out the form...
+        const fieldNames = pdfDoc
+            .getForm()
+            .getFields()
+            .map((f) => f.getName());
+        console.log({ fieldNames });
 
+        const form = pdfDoc.getForm();
+        
         const pdfBytes = await pdfDoc.save();
 
         await writeFile(output, pdfBytes);
@@ -17,4 +26,4 @@ async function createPdf(input, output) {
     }
 }
 
-createPdf('')
+createPdf('./forms/work-order-form.pdf', './forms/output.pdf');
